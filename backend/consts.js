13aeb6c8e1433467898d9a48127e629d78527eb7
rafinha285 <jcschrott10@gmail.com>
2024-04-03 23:29:43 -0300

    build and https path

--- backend/consts.js
+++ backend/consts.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.IP_DATABASE = exports.RESOLUTIONS = exports.FFPROBE_PATH = exports.FFMPEG_PATH = exports.TEMP_PATH = exports.MANGA_PATH = exports.ANIME_PATH = void 0;
+exports.IP_DATABASE = exports.HTTPS_CERT_PATH = exports.HTTPS_KEY_PATH = exports.BUILD_PATH = exports.RESOLUTIONS = exports.FFPROBE_PATH = exports.FFMPEG_PATH = exports.TEMP_PATH = exports.MANGA_PATH = exports.ANIME_PATH = void 0;
 var path = require("path");
 exports.ANIME_PATH = path.join("D:", "main", "server", "Anime");
 exports.MANGA_PATH = path.join("D:", "main", "server", "Manga");
@@ -8,7 +8,6 @@ exports.TEMP_PATH = path.join("D:", "main", "server", "temp");
 exports.FFMPEG_PATH = path.join("D:", "main", "ffmpeg", "bin", "ffmpeg.exe");
 exports.FFPROBE_PATH = path.join("D:", "main", "ffmpeg", "bin", "ffprobe.exe");
 exports.RESOLUTIONS = ['1920x1080', '1280x720', '854x480'];
-exports.IP_DATABASE = '192.168.1.20';
 exports.BUILD_PATH = path.join(__dirname, '../', 'build');
 exports.HTTPS_KEY_PATH = path.join(__dirname, "../", "../", "https", 'chave.pem');
 exports.HTTPS_CERT_PATH = path.join(__dirname, "../", "../", "https", 'certificado.pem');

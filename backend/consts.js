3884e88e33ecc39550fc28b51c525659f40fcc9e
rafinha285 <jcschrott10@gmail.com>
2024-04-03 23:09:07 -0300

    consts

--- backend/consts.js
+++ backend/consts.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.IP_DATABASE = exports.RESOLUTIONS = exports.FFPROBE_PATH = exports.FFMPEG_PATH = exports.TEMP_PATH = exports.MANGA_PATH = exports.ANIME_PATH = void 0;
 var path = require("path");
-exports.ANIME_PATH = path.join("D:", "main", "server", "Anime");
-exports.MANGA_PATH = path.join("D:", "main", "server", "Manga");
-exports.TEMP_PATH = path.join("D:", "main", "server", "temp");
-exports.FFMPEG_PATH = path.join("D:", "main", "ffmpeg", "bin", "ffmpeg.exe");
-exports.FFPROBE_PATH = path.join("D:", "main", "ffmpeg", "bin", "ffprobe.exe");
+exports.ANIME_PATH = path.join("~/", 'storage', 'anime');
+exports.MANGA_PATH = path.join("~/", 'storage', 'manga');
+exports.TEMP_PATH = path.join("~/", 'storage', 'temp');
+exports.FFMPEG_PATH = path.join('usr', 'bin', 'ffmpeg');
+exports.FFPROBE_PATH = path.join('usr', 'bin', 'ffprobe');
 exports.RESOLUTIONS = ['1920x1080', '1280x720', '854x480'];
-exports.IP_DATABASE = '192.168.1.20';
+exports.IP_DATABASE = 'localhost';

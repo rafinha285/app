512864704722a257f23350a3aaca155c76464dd6
rafinha285 <jcschrott10@gmail.com>
2024-04-25 16:50:57 -0300

    types

--- src/functions/animeFunctions.ts
+++ src/functions/animeFunctions.ts
@@ -2,7 +2,7 @@ import $ from "jquery"
 import {Anime, Producer,Season} from "../types/animeModel"
 import AnimePoster from "../assets/AnimePosters"
 import { Episode } from "../types/episodeModel";
-import { languages } from "../types/types.ts";
+import { languages } from "../types/episodeModel";
 
 
 export function genToArray(gen:string){

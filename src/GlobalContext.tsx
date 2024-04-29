b52467597039300eda6a97eb7be4549b1b3e6bf4
rafinha285 <jcschrott10@gmail.com>
2024-04-29 01:50:27 -0300

    useCookies

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -1,27 +1,19 @@
 import React, { createContext, ReactNode, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 // import jwt from 'jsonwebtoken';
 
 interface GlobalContextType {
     isLogged: boolean;
 }
 const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
-const getCookie = (name: string): string | null => {
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.split('=');
-      if (cookieName.trim() === name) {
-        return cookieValue;
-      }
-    }
-    return null;
-};
+
 export const GlobalProvider:React.FC<{children:ReactNode}> = ({children}) =>{
     const [isLogged, setIsLogged] = useState<boolean>(false)
     // const token = getCookie('token');
     useEffect(() => {
-        const token = getCookie('token');
-        console.log(token)
-        setIsLogged(!!token); // Verifica se o token existe e define o estado de isLogged
+        const [cookies] = useCookies(['token']);
+        console.log(cookies.token)
+        setIsLogged(!!(cookies.token)); // Verifica se o token existe e define o estado de isLogged
     }, []); 
     
     return(

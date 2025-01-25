52e02c818a3b12b0313b83f2c5a7e510f0d562a1
Rafael <54754297+rafinha285@users.noreply.github.com>
2025-01-24 23:10:53 -0300

    CharacterDiv.tsx added

---
+++ src/components/Anime/CharacterDiv.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PersoCompo from "../Perso";
+import {Character} from "../../types/Character";
+
+interface Props{
+    characters:Character[];
+    aniId:string;
+}
+
+const CharacterDiv:React.FC<Props> = ({characters,aniId}) =>{
+    return (
+        <div className="personagens">
+            <div style={{display: "flex", justifyContent: "space-between", marginBottom: "1em"}}>
+                <h1>Personagens: </h1>
+            </div>
+            <div style={{
+                border: "1px white solid",
+                padding: "1em",
+                display: "flex",
+                flexWrap: "nowrap",
+                justifyContent: "flex-start",
+                flexDirection: 'row',
+                overflow: "auto"
+            }}>{(characters??[]).map((v, i) => (
+                <PersoCompo perso={v} aniId={aniId} key={i}></PersoCompo>
+            ))}</div>
+        </div>
+    )
+}
+export default CharacterDiv
